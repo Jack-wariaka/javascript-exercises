0319f1f2b8e7013fb63b32364bac26da70246109
majack <jackwariaka@gmail.com>
2026-07-13 21:34:01 +0300

    did assignments on javascript arrays and loops from exercise 6 to 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  tempInFarh = (temp - 32) * 5/9; 
+  tempInFarh = Number(tempInFarh.toFixed(1));
+  return tempInFarh;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  tempInCelcius = temp * 9/5 + 32
+  tempInCelcius = Number(tempInCelcius.toFixed(1));
+  return tempInCelcius;
 };
 
 // Do not edit below this line
